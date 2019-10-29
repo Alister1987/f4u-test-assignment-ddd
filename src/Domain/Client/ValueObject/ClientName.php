dedfbbc03f45d0a3c31c35d0143f26580c8b9d81
Alex Strelnik <alisterr1987@gmail.com>
2019-10-29 22:43:23 +0200

    Added all project stuff

---
+++ src/Domain/Client/ValueObject/ClientName.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Client\ValueObject;
+
+class ClientName
+{
+    private $firstName;
+
+    private $lastName;
+
+    public function __construct(string $firstName, string $lastName)
+    {
+        //@todo: need add validation for check empty field
+
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    public function __toString()
+    {
+        return "{$this->firstName()} {$this->lastName()}";
+    }
+
+    public function firstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function lastName(): string
+    {
+        return $this->lastName;
+    }
+}
